d9fb06fe702371b3317d60e4b956100b55eb43c2
Matt <matiklimczak@gmail.com>
2023-09-06 10:53:40 +0200

    day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var age = 33;
+var name = "Ewa";
+var gender = "kobieta";
+
+if (age < 30 && gender == "kobieta")
+{
+    Console.WriteLine("Kobieta poniezj 30 lat");
+}
+
+else if (name == "Ewa" && age == 33)
+
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
